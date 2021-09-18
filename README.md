9d3c66cffca6b6bf0ed121aa6b0ae67347f11cab
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-09-18 18:50:03 +0100

    add get billable by month/total
    todo refactor

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 Query time invested via api (using fastapi).
 
-
 # Usage
 
 https://time.karmacomputing.co.uk/#/default/read_item_tmetric_timeentries_post
@@ -14,3 +13,12 @@ Enter your user id and Tmetric account id (same for everyone)
 #### how do I find my user id / account id
 See example API request image which highlights user id and account id.
 https://tmetric.com/help/data-integrations/how-to-use-tmetric-rest-api
+# Run locally
+
+```
+python3 -m venv venv
+pip install -r requirements.txt
+. venv/bin/activate
+uvicorn main:app --reload
+```
+Visit http://127.0.0.1:8000
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 import os
 from dotenv import load_dotenv
-from datetime import date
+from datetime import date, datetime
+from datetime import timedelta
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+import calendar
+
 
 description = "Get time invested"
 
@@ -15,12 +18,19 @@ load_dotenv(verbose=True)
 API_HOST = os.getenv("API_HOST", "https://app.tmetric.com/api/v3/")
 API_TOKEN = os.getenv("API_TOKEN")
 
+RATE_PER_MIN = float(os.getenv("RATE_PER_MIN"))
+
 
 class TimeEntries(BaseModel):
     user_id: int
     account_id: int
     date_start: Optional[date] = None
     date_end: Optional[date] = None
+
+
+class TmetricAccount(BaseModel):
+    user_id: int
+    account_id: int
 
 
 def getTimeEntries(timeEntries: TimeEntries):
@@ -45,6 +55,144 @@ def getTimeEntries(timeEntries: TimeEntries):
     return req.json()
 
 
+def getTotalUserBillableThisMonth(user_id, account_id, rate_per_min=0.75):
+    """Calculate the totall billable this month for a user
+    return: json
+        - The number of minutes invested
+        - Number of hours invested
+        - Amount billable in smallest unit (e.g. pennies)
+        - Amount billable in human readable (e.g. £ and pence)
+        {
+          "totalHours": 54,
+          "totalMinutes": 454,
+          "billable": 100,
+          "billable-human-readable": "£1",
+          "ratePerMin": RATE_PER_MIN,
+        }
+    """
+
+    USER_ID = user_id
+    ACCOUNT_ID = account_id
+    totalTime = timedelta()
+
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}",
+        "account": "text/plain",
+    }
+
+    today = date.today()
+
+    startDate = today.strftime("%Y-%m-1")  # Always first day of current month
+
+    last_day = calendar.monthrange(today.year, int(today.month))[1]
+
+    endDate = today.strftime(f"%Y-%m-{last_day}")
+
+    api_call = f"{API_HOST}accounts/{ACCOUNT_ID}/timeentries?userId={USER_ID}&startDate={startDate}&endDate={endDate}"  # noqa: E501
+    try:
+        req = requests.get(
+            api_call,
+            headers=headers,
+        )
+    except requests.exceptions.ConnectionError as e:
+        return f"Connection error to tmetric. {e}"
+
+    resp = req.json()
+    for entry in resp:
+        startTime = datetime.strptime(entry["startTime"], "%Y-%m-%dT%H:%M:%S")
+
+        endTime = datetime.strptime(entry["endTime"], "%Y-%m-%dT%H:%M:%S")
+
+        diff = endTime - startTime
+        totalTime += diff
+
+    def calculateBillable(seconds, ratePerMin):
+        return seconds / 60 * RATE_PER_MIN
+
+    billable = calculateBillable(totalTime.seconds, RATE_PER_MIN)
+    return {
+        "totalMinutes": int(totalTime.seconds / 60),
+        "totalHours": totalTime.seconds / 60 / 60,
+        "billable-pounds": billable,
+        "billable-pennies": int(billable * 100),
+        "billable-human-readable": f"£{billable}",
+        "ratePerMin": RATE_PER_MIN,
+    }
+
+
+def getTotalUserBillableByMonth(
+    user_id: int, account_id: int, rate_per_min: int, month: int, year: int
+):
+    USER_ID = user_id
+    ACCOUNT_ID = account_id
+    totalTime = timedelta()
+
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}",
+        "account": "text/plain",
+    }
+
+    startDate = date(year, month, 1)  # Always from the first date of the month
+
+    last_day = calendar.monthrange(year, month)[1]
+
+    endDate = startDate.strftime(f"%Y-%m-{last_day}")
+
+    api_call = f"{API_HOST}accounts/{ACCOUNT_ID}/timeentries?userId={USER_ID}&startDate={startDate}&endDate={endDate}"  # noqa: E501
+    try:
+        req = requests.get(
+            api_call,
+            headers=headers,
+        )
+    except requests.exceptions.ConnectionError as e:
+        return f"Connection error to tmetric. {e}"
+
+    resp = req.json()
+    for entry in resp:
+        startTime = datetime.strptime(entry["startTime"], "%Y-%m-%dT%H:%M:%S")
+
+        endTime = datetime.strptime(entry["endTime"], "%Y-%m-%dT%H:%M:%S")
+
+        diff = endTime - startTime
+        totalTime += diff
+
+    def calculateBillable(seconds, ratePerMin):
+        return seconds / 60 * RATE_PER_MIN
+
+    billable = calculateBillable(totalTime.seconds, RATE_PER_MIN)
+    return {
+        "totalMinutes": int(totalTime.seconds / 60),
+        "totalHours": totalTime.seconds / 60 / 60,
+        "billable-pounds": billable,
+        "billable-pennies": int(billable * 100),
+        "billable-human-readable": f"£{billable}",
+        "ratePerMin": RATE_PER_MIN,
+        "tmetric_raw": resp,
+    }
+
+
 @app.post("/tmetric-timeentries")
 async def read_item(timeEntries: TimeEntries):
     return getTimeEntries(timeEntries)
+
+
+@app.get("/total-user-billable-this-month")
+async def total_user_billable_this_month(
+    user_id: int, account_id: int, rate_per_min: Optional[int] = None
+):
+    return getTotalUserBillableThisMonth(
+        user_id, account_id, rate_per_min=rate_per_min
+    )  # noqa: E501
+
+
+@app.get("/total-user-billable-by-month")
+async def total_user_billable_by_month(
+    user_id: int,
+    account_id: int,
+    month: int,
+    year: Optional[int] = datetime.today().year,
+    rate_per_min: Optional[int] = None,
+):
+    return getTotalUserBillableByMonth(
+        user_id, account_id, rate_per_min=rate_per_min, month=month, year=year
+    )  # noqa: E501
